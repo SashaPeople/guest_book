771b758b5ee0967442348d66adeef3b4f471c752
sashapeople <sashapeople@gmail.com>
2018-05-09 01:15:37 +0300

    changed the name of the role

--- connect.php
+++ connect.php
@@ -1,8 +1,8 @@
 <?php
 $host = 'localhost';
 $port = '5432';
-$dbname = 'testdb';
-$user = 'sp';
+$dbname = 'postgres';
+$dbuser = 'postgres';
 $password = '4603';
-$db = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password") or exit("Невозможно соединиться с сервером");
+$db = pg_connect("host=$host port=$port dbname=$dbname user=$dbuser password=$password") or exit("Невозможно соединиться с сервером");
 ?>
